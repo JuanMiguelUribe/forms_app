9509c8568675d48d2313e902df21097469f47994
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-21 21:43:32 -0500

    getter de error message

--- lib/infrastructure/inputs/password.dart
+++ lib/infrastructure/inputs/password.dart
@@ -10,6 +10,17 @@ class Password extends FormzInput<String, PasswordError> {
 
   // Call super.dirty to represent a modified form input.
   const Password.dirty(String value) : super.dirty(value);
+  String? get errorMessage {
+    if (isValid || isPure) return null;
+
+    if (displayError == PasswordError.empty) {
+      return "Campo Requerido";
+    } else if (displayError == PasswordError.length) {
+      return "Minimo 6 caracteres";
+    } else {
+      return null;
+    }
+  }
 
   // Override validator to handle validating a given input value.
   @override
